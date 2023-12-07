bc3571d34c3726abcf754c58745d6da27bcd66e8
Kupasva <130740583+Kupasva@users.noreply.github.com>
2023-12-07 16:24:38 +0530

    answer

---
+++ question3/answer.py
@@ -0,0 +1,14 @@
+# it's calculator with +, -, *, /, % in with a two interger number.
+Number_1 = int(input("Enter a 1st_number for caculate: "))
+Number_2 = int(input("Enter a 2nd_number for caculate: "))
+
+result1 = Number_1 + Number_2
+print(f"Addition of two numbers is {result1}.")
+result2 = Number_1 - Number_2
+print(f"Subtarction of two number is {result2}.")
+result3 = Number_1 * Number_2
+print(f"Multiplication of two number is {result3}.")
+result4 = Number_1 / Number_2
+print(f"Division of two number is {result4}.")
+result5 = Number_1 % Number_2
+print(f"Moduls of two number is {result5}.")
