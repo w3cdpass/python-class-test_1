3769972caae366d59bacfff8b2504a6b479a6039
Kupasva <130740583+Kupasva@users.noreply.github.com>
2023-12-08 13:20:09 +0530

    un-understantable question

--- question2/answer.py
+++ question2/answer.py
@@ -17,10 +17,6 @@ print(F"{Employee_name} 12% of PF is {Pro_fund}.")
 hra = Basic_Salary * 20 / 100  # but maximum can be 2000 only kaise kare 
 print(f"{Employee_name} your HRA is {hra}.")
 
-''' da is 20% of bp for bp < 3 lac
-    otherwise
-    da is 30% of bp 
-'''
 Dearness_Allowance = Basic_Salary * 20 /100
 print(f"{Employee_name} your Da is {Dearness_Allowance}.")
 
@@ -38,3 +34,10 @@ print(f"{Employee_name} your net pay is {Net_pay}.")
 
 
 # Dearness_Allowance = 9
+
+'''
+    (hra is 20% of bp but maximum can be 2000 only) ~~ isme function ya method konsa use hoga 
+    (da is 20% of bp for bp < 3 lac
+    otherwise                                       ~~ mujhe ye question smaj nhi aya sir ji  
+    da is 30% of bp )
+'''
